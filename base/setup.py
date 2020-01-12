b13b9ddc4a6f459f923ad087bc27ea7fd12c33f1
M Nasrul Alawy <alphacsoft@gmail.com>
2020-01-12 16:00:34 +0700

    add redirect function to regius

--- base/setup.py
+++ base/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="regius", 
-    version="0.0.2",
+    version="0.0.3",
     author="M Nasrul Alawy",
     author_email="alawy.nasrul@gmail.com",
     description="regius - Python Handsome Web Framework",
